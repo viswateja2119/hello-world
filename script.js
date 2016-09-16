2d3e8dab2a5148e0bb365aa9bb1c128099c3d447
viswateja2119 <viswateja2119@gmail.com>
2016-09-16 00:12:31 -0400

    added two new files UI

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+var myApp= angular
+                  .module("myModule",[])
+				  .controller("myController",function($scope){
+					  
+					  $scope.message="Hello Angular!";
+					  
+				  });
